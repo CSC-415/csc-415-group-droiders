4dc54e234ec68edd12966613eef036286cbd5be6
roym5 <mattman2015@ymail.com>
2023-02-28 00:39:32 -0500

    added trex image with a line underneath it with glide dependencies added a basic view attached to main that shows a default scoring changed the view to be forced landscape

--- app/src/main/java/com/example/groupproject/MainActivity.kt
+++ app/src/main/java/com/example/groupproject/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.groupproject
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val dinoImageView = findViewById<ImageView>(R.id.dino)
+
+
+        Glide
+            .with(this)
+            .load(R.drawable.trex)
+            .into(dinoImageView)
+
+        val scoreTextView = findViewById<TextView>(R.id.current_score)
+        scoreTextView.text = getString(R.string.scoreBeforeStart)
+
+        val hiscoreTextView = findViewById<TextView>(R.id.high_score)
+        hiscoreTextView.text=getString(R.string.hiscore) //later would update score
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        /*
+        potential logic for handling running total
+        var count = 0
+        count++
+        val scoreTextView = findViewById<TextView>(R.id.current_score)
+        scoreTextView.text = "Count: $count"
+        */
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
