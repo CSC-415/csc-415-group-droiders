a05c7b721124a6a17cd7c6840097619d421be106
Clementine Farrell <71611480+farrell22@users.noreply.github.com>
2023-04-20 12:29:59 -0400

    updated leaderboard fragment to print nums

--- app/src/main/java/com/example/groupproject/LeaderboardFragment.kt
+++ app/src/main/java/com/example/groupproject/LeaderboardFragment.kt
@@ -1,4 +1,31 @@
 package com.example.groupproject
 
-class LeaderboardFragment {
+import com.example.groupproject.data.leaderboard.ScoresData
+
+import com.example.groupproject.Game
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class LeaderboardFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.leaderboard, container, false)
+    }
+
+    init {
+        val newScore = 0
+        val scoresData = context?.let { ScoresData(newScore, it) }
+        val scoresArr = scoresData?.getScoresArr()
+
+        for (i in 0 until 5) {
+            println("${i + 1}. ${scoresArr?.get(i)}")
+        }
+    }
 }
